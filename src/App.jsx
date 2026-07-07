80101b6ddd40febd924038e4a9d0063aed7f211d
baam <fer.barron.alvarez.16@gmail.com>
2026-07-06 22:53:08 -0600

    :sparkles: Basic structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+export default function App() {
+  return (
+    <main>
+      {/*Gradient image*/}
+      <img className="absolute top-0 right-0 opacity-60 -z-10" src="/gradient.png" alt="Gradient-img" />
+
+      {/*Blur effect*/}
+      <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+
+      <Header/>
+    </main>
+  )
+}
