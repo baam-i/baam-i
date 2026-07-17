7fcc6c15051da4e070b8556e4c2fbaf2036bd3d0
baam <fer.barron.alvarez.16@gmail.com>
2026-07-17 01:49:42 -0600

    Configuring base for github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,8 +13,8 @@ const Header = () => {
     }
   return (
     <header className="flex justify-between items-center py-4 px-4 lg:px-20">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-light m-0">
-            BAAM
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold m-0">
+            BaAm
         </h1>
         {/* Desktop Navigation */}
         <nav className="hidden md:flex items-center gap-12">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
         <div className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
             <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#9246b3] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
                 <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
-                    <i class='bx bx-diamond' ></i> INTRODUCING
+                    <i class='bx bx-diamond' ></i> I'm ...
                 </div>
             </div>
 
             {/*main heading*/}
             <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold tracking-wider my-8'>
-                EMAIL FOR
+                jr
                 <br />
-                DEVELOPERS
+                dev
             </h1>
 
             {/*description*/}
@@ -26,11 +26,11 @@ const Hero = () => {
             {/*buttons*/}
             <div className='flex gap-4 mt-15 my-4'>
                 <a className='border border-[#a2a2a2] py-2 sm:py-3 px-4 sm:px-5 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#9246b3]' href='#'>
-                    documentation <i class='bx bx-link-alt'></i>
+                    idk i followed a tutorial <i class='bx bx-link-alt'></i>
                 </a>
                 
                 <a className='border border-[#a2a2a2] py-2 sm:py-3 px-4 sm:px-10 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#9246b3] bg-gray-300 text-black hover:text-white' href='#'>
-                    getStarted <i class='bx bx-link-alt'></i>
+                    same <i class='bx bx-link-alt'></i>
                 </a>
             </div>
         </div>
@@ -38,8 +38,6 @@ const Hero = () => {
         {/*3d*/}
         <Spline  className='absolute lg:top-0 top-[-1%] bottom-0 lg:left-[25%] sm:left-[-2%] h-full'
         scene="https://prod.spline.design/zvPRF0WC0nwCD8ys/scene.splinecode" />
-        
-
     </main>    
   )
 }
